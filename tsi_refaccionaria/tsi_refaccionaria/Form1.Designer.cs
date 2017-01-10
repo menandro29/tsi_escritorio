8e35a671f7e766ee52906f6488e27189caadfb14
menandro29 <menandro29@hotmail.com>
2017-01-10 16:59:38 -0600

    TextBox

--- tsi_refaccionaria/tsi_refaccionaria/Form1.Designer.cs
+++ tsi_refaccionaria/tsi_refaccionaria/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -50,11 +51,19 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "PASSWORD";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(344, 290);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -68,6 +77,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
